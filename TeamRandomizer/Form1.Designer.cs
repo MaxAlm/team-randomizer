116cffb5640186bc3e908b6f54bce8d101ed9b72
Max Almroth <max.almroth95@gmail.com>
2021-11-04 09:13:56 +0100

    add files to github

---
+++ TeamRandomizer/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace TeamRandomizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button_randomize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(388, 578);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button_randomize
+            // 
+            this.button_randomize.Location = new System.Drawing.Point(263, 616);
+            this.button_randomize.Name = "button_randomize";
+            this.button_randomize.Size = new System.Drawing.Size(137, 39);
+            this.button_randomize.TabIndex = 1;
+            this.button_randomize.Text = "Randomize";
+            this.button_randomize.UseVisualStyleBackColor = true;
+            this.button_randomize.Click += new System.EventHandler(this.button_randomize_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 682);
+            this.Controls.Add(this.button_randomize);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Team Randomizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button_randomize;
+    }
+}
+
---
+++ TeamRandomizer/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeamRandomizer
+{
+    public partial class Form1 : Form
+    {
+        // METHODS
+        
+        
+
+        // VARIABLES
+        
+        List<string> names = new List<string>();
+        
+        // CODE START
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_randomize_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
